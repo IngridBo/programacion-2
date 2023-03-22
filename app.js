02e7c3c042099544b0efbefc2b49389a64e5edb0
IngridBonilla <Bonilla.1ingrid@gmail.com>
2023-03-22 15:50:11 -0500

    add ejercicio

---
+++ app.js
@@ -0,0 +1,8 @@
+
+let celsius = prompt("Ingresa la temperatura en grados Celsius");
+
+let fahrenheit = (celsius * 9/5) + 32;
+let kelvin = (celsius + 273.15);
+
+
+console.log(celsius + "°C equivalen a " + fahrenheit + "°F y " + kelvin + "°K");
